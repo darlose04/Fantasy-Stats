a380a1ce03b42c126648b9f30bd715365e99af8b
Zach Smith <zsmith126028@gmail.com>
2018-09-25 22:09:02 -0500

    add required packages

--- app.js
+++ app.js
@@ -1,39 +1,25 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    User = require("./models/user");
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-var teams = [
-  {
-    name: "YABO CITY",
-    image:
-      "https://www.baseballprospectus.com/wp-content/uploads/2018/01/mlb-baseball.jpeg"
-  },
-  {
-    name: "Bryce Bryce Baby",
-    image:
-      "https://www.baseballprospectus.com/wp-content/uploads/2018/01/mlb-baseball.jpeg"
-  },
-  {
-    name: "Yu Eaton Dickie",
-    image:
-      "https://www.baseballprospectus.com/wp-content/uploads/2018/01/mlb-baseball.jpeg"
-  },
-  {
-    name: "Disabled List",
-    image:
-      "https://www.baseballprospectus.com/wp-content/uploads/2018/01/mlb-baseball.jpeg"
-  }
-]
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
 app.get("/teams", function(req, res) {
-  res.render("teams", {teams: teams});
+  res.render("teams");
 });
 
 app.listen(2500, function() {
