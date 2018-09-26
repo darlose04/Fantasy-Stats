38576461d6a7c037cab291d2f5b8a26496a4cc7a
Zach Smith <zsmith126028@gmail.com>
2018-09-26 10:11:46 -0500

    add create route for discussion

--- routes/discussions.js
+++ routes/discussions.js
@@ -15,3 +15,29 @@ router.get("/", function(req, res) {
   });
 });
 
+// CREATE - add a new discussion to the database
+router.post("/", middleware.isLoggedIn, function(req, res) {
+  // get data from form and add to the discussions array
+  var name = req.body.name;
+  var desc = req.body.description;
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  };
+  var newDiscussion = {
+    name: name,
+    description: desc,
+    author: author
+  };
+  // create new discussion and save to the db
+  Discussion.create(newDiscussion, function(err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      // redirect back to discussion page
+      console.log(newlyCreated);
+      res.redirect("/discussions");
+    }
+  });
+});
+
