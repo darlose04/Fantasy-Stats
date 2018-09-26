cbd775138580655a72f632a06499d06817fd91d9
Zach Smith <zsmith126028@gmail.com>
2018-09-26 10:07:38 -0500

    add index route for discussions

--- routes/discussions.js
+++ routes/discussions.js
@@ -3,3 +3,15 @@ var router = express.Router();
 var Discussion = require("../models/discussion");
 var middleware = require("../middleware");
 
+// INDEX - show discussions
+router.get("/", function(req, res) {
+  // get the discussions from the database
+  Discussion.find({}, function(err, allDiscussions) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("discussions/index", { discussions: allDiscussions});
+    }
+  });
+});
+
