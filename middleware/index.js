971fbd6528c0655de6edcc8002d4afa6ca7215b8
Zach Smith <zsmith126028@gmail.com>
2018-09-25 23:09:42 -0500

    add checkCommentOwnership middleware

--- middleware/index.js
+++ middleware/index.js
@@ -27,3 +27,26 @@ middlewareObj.checkDiscussionOwnership = function(req, res, next) {
     res.redirect("back");
   }
 }
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+  // is user logged in?
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment._id, function(err, foundComment) {
+      if (err || !foundComment) {
+        req.flash("error", "Comment not found");
+        res.redirect("back");
+      } else {
+        // does user own comment?
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          req.flash("error", "You do not have persmission to do that");
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    req.flash("error", "You need to be logged in to do that");
+    res.redirect("back")
+  }
+}
