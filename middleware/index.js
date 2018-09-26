99dd0954ccaeeaae8b162e3e4ac3255c7909050d
Zach Smith <zsmith126028@gmail.com>
2018-09-25 23:05:06 -0500

    add middleware index file. add checkDiscussionOwnership middleware

---
+++ middleware/index.js
@@ -0,0 +1,29 @@
+// putting all middleware in the directory
+
+var Discussion = require("../models/discussion");
+var Comment = require("../models/comment");
+
+var middlewareObj = {};
+
+middlewareObj.checkDiscussionOwnership = function(req, res, next) {
+  // is user logged in?
+  if (req.isAuthenticated()) {
+    Discussion.findById(req.params.id, function(err, foundDiscussion) {
+      if(err || !foundDiscussion) {
+        req.flash("error", "Discussion not found");
+        res.redirect("back");
+      } else {
+        // does user own discussion?
+        if (foundDiscussion.author.id.equals(req.user._id)) { // use .equals: first id is mongoose id, second id is string id: == or === does not work
+          next();
+        } else {
+          req.flash("error", "You do not have permission to do that");
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    req.flash("error", "You need to be logged in to do that");
+    res.redirect("back");
+  }
+}
