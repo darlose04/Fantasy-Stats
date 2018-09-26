2deb0037153c27bb4268f7436b3c14d7de5f9df7
Zach Smith <zsmith126028@gmail.com>
2018-09-25 22:53:57 -0500

    add team model

---
+++ models/team.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+var teamSchema = new mongoose.Schema({
+  name: String,
+  AVG: Number,
+  R: Number,
+  HR: Number,
+  RBI: Number,
+  SB: Number,
+  OPS: Number,
+  IP: Number,
+  SV: Number,
+  K: Number,
+  ERA: Number,
+  WHIP: Number,
+  QS: Number
+});
+
+module.exports = mongoose.model("Team", teamSchema);
