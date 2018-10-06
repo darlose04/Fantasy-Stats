98c8736a119db8cc748bb8c3017326c4b9b2f411
Zach Smith <zsmith126028@gmail.com>
2018-10-06 17:58:21 -0500

    add routes for teams

---
+++ routes/teams.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var router = express.Router();
+var Team = require("../models/team");
+
+// INDEX - show teams
+router.get("/", function(req, res) {
+  // get the teams from the database
+  Team.find({}, function(err, allTeams) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("teams/index", {teams: allTeams});
+    }
+  });
+});
+
+// SHOW info about one team
+router.get("/:id", function(req, res) {
+  // find team with provided id
+  Team.findById(req.params.id).exec(function(err, foundTeam) {
+    if (err || !foundTeam) {
+      req.flash("error", "Team not found");
+      res.redirect("back");
+    } else {
+      console.log(foundTeam);
+      // render show template with that team
+      res.render("teams/show", {team: foundTeam});
+    }
+  });
+});
+
+module.exports = router;
